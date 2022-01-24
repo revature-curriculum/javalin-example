be37babf3aa2da70111d1c76695e81bfda1091d6
aviflombaum <>
2022-01-24 21:14:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("You are the wind beneath my wings!"));
+    }
+}
